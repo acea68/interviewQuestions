01e0f65ed06540d252595b914650832c79fe4165
acea68 <alancea01@gmail.com>
2022-07-24 09:09:33 -0500

    updates pramp ints

--- pramp/validateIp.js
+++ pramp/validateIp.js
@@ -23,11 +23,35 @@ Constraints:
 [output] boolean
 */
 
+console.time('function1');
 function validateIP(ip) {
-	/**
-	@param ip: string
-	@return: boolean
-	*/
-
-	// your code goes here
+  /**
+   @param ip: string
+   @return: boolean
+  c: 0 >= nums >= 255, only three periods,
+  e: check for chars other then int, after parsing (int only),
+  */
+  // split ip at '.' & store arr
+  // if arr.length !== 4, return false
+  // parse each elem to nums
+  // if each elem typeof !== num, return false
+  // if nums not >=0 & <= 255, return false
+  // return true
+  let ipArray = ip.split('.');
+  if (ipArray.length !== 4) return false;
+  for (let subNum of ipArray) {
+    let segment = parseInt(subNum);
+    if (!Number.isInteger(segment) || (0 > segment || 255 < segment)) return false;
+  }
+  return true
 }
+console.timeEnd('function1');
+
+// var ip = '192.168.0.1'; // true
+// var ip = '0.0.0.0'; // true
+// var ip = '123.24.59.99'; // true
+// var ip = '192.168.123.456'; // false
+// var ip = '1.2.3.4.5'; // false
+var ip = "12.34.56.oops"; // false
+
+console.log('🚀 ~ validateIP', validateIP(ip));
